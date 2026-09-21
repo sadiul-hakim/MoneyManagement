cc8589665c3ae9b999bf05e40aefc7752fc27df8
Sadiul Hakim <sadiulhakim@gmail.com>
2026-09-21 11:32:02 +0600

    media

--- MoneyManagement/urls.py
+++ MoneyManagement/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for MoneyManagement project.
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +9,7 @@ urlpatterns = [
     path('', include('pwa.urls')),
     path('', include('main.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
